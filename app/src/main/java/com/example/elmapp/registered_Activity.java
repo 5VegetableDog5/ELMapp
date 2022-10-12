d8d88deb9486ff116ecda666970878ba3111e060
vegetabledog <2286224001@qq.com>
2022-10-12 19:33:05 +0800

    初始化仓库

---
+++ app/src/main/java/com/example/elmapp/registered_Activity.java
@@ -0,0 +1,14 @@
+package com.example.elmapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class registered_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registered);
+
+    }
+}
